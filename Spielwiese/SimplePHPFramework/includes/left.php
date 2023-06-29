fabb5e4152ef90682f4d1d4bacb06e16c1e99420
Alexander Thiess <thiess.alexander@googlemail.com>
2023-06-29 15:00:00 +0200

    fixed links.

--- Spielwiese/SimplePHPFramework/includes/left.php
+++ Spielwiese/SimplePHPFramework/includes/left.php
@@ -1,3 +1,3 @@
-<a href="?content=customers">Customers</a><br />
-<a href="?content=customers">ingredients</a><br />
-<a href="?content=customers">recipes</a><br />
+<a href="index.php?content=customers">Customers</a><br />
+<a href="index.php?content=customers">ingredients</a><br />
+<a href="index.php?content=customers">recipes</a><br />
