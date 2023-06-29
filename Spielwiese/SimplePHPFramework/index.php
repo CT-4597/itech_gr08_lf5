11c8914ea128ac7c414332ef5302ebf6d28c2573
Alexander Thiess <thiess.alexander@googlemail.com>
2023-06-29 11:37:49 +0200

    Spielwiese first commit

---
+++ Spielwiese/SimplePHPFramework/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Kraut & R&ubnsp;ben</title>
+</head>
+<body>
+<table border="1">
+	<tr>
+		<td>header</td>
+	</tr>
+	<tr>
+		<td>left</td>
+		<td>mid</td>
+		<td>right</td>
+	</tr>
+</table>
+</body>
+</html>
