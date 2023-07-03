6acc9ccd4e6819dbf79f0ec484c452d1b8cccc01
Simon Niclas Stambor <stambor97@gmail.com>
2023-07-03 12:41:31 +0200

    connected to database and first navigation layout

--- Spielwiese/SimplePHPFramework/includes/config.php
+++ Spielwiese/SimplePHPFramework/includes/config.php
@@ -1,3 +1,14 @@
 <?php
-	# sql configurating would be here
+// Database parameter
+$servername = "localhost";
+$username = "g8";
+$password = "#Kennwort10!";
+$dbname = "krautundrueben";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
--- Spielwiese/SimplePHPFramework/index.php
+++ Spielwiese/SimplePHPFramework/index.php
@@ -1,31 +1,45 @@
-<?php
-	include('includes/config.php');
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Kraut & R&ubnsp;ben</title>
+	<title>Kraut & Rüben</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<table border="1" width="100%">
-	<tr>
-		<td colspan="3"><?php include('includes/header.php'); ?></td>
-	</tr>
-	<tr>
-		<td><?php include('includes/left.php'); ?></td>
-		<td>
-			<?php
-			if(isset($_GET['content'])) {
-				include('includes/content_' . htmlspecialchars($_GET["content"]) . '.php');
-			} else {
-				include('includes/content_customers.php');
-			}
-			?>
-		</td>
-		<td><?php include('includes/right.php'); ?></td>
-	</tr>
-</table>
+
+    <div class="header">
+        <img src="/images/Logo.png" class="logo">
+        <h1>KuR-Verwaltung</h1>
+        <img src="/images/user_icon.png" class="user_icon">
+    </div>
+    <div class="navigation">
+        <a href="/index.php" class="navitem">Zutaten</a>
+        <a href="/index.php" class="navitem">Rabatt-Boxen</a>
+        <a href="/index.php" class="navitem">Bio-Boxen</a>
+        <a href="/index.php" class="navitem">Rezept-Boxen</a>
+    </div>
+
+    <div class="content">
+<?php
+include './includes/config.php';
+$sql = "SELECT * FROM ALLERGEN";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Bezeichnung</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["ALLERGENNR"]. "</td><td>" . $row["ALLERGENBEZEICHNUNG"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+    </div>
+
 </body>
 </html>
