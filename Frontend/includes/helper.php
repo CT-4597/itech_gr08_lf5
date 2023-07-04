29b04871d4727d50174fe64074eca1b752cec3bd
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-04 17:56:07 +0200

    Helper Function added. ImagePath

---
+++ Frontend/includes/helper.php
@@ -0,0 +1,17 @@
+<?php
+	/*
+	Function call:
+		get_image_path([ImageType], [ID]);
+
+	Example:
+		<img src="<?php get_image("z",1001);?>">
+    */
+function get_image($type, $id){
+	$image_path = "/images/" . $type . "_" . $id . ".png";
+	if(file_exists($image_path)){
+		echo $image_path;
+	} else {
+		echo "/images/noimage.svg";
+	}
+}
+?>
--- Frontend/index.php
+++ Frontend/index.php
@@ -1,5 +1,6 @@
 <?php
     include('includes/config.php');
+    include('includes/helper.php');
     include('includes/sql_logger.php');
 
     # Check for page var. Get the default page if needed.
