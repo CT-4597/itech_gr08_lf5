5a01f6a6802e25f202a1645198f5c1e89dce004e
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-05 10:33:02 +0200

    added redirect

--- Frontend/content/login.php
+++ Frontend/content/login.php
@@ -1,6 +1,8 @@
 <?php
   if(isset($_POST['customer']) && isset($_POST['passwd'])) {
     debug_log("Login as " . $_POST['customer']);
+    header("Location: /");
+    exit();
   }
  ?>
 <form action="/login" method="post">
