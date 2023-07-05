fb60417a89c5a66e6788545abcf17c639424a9c0
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-05 10:27:36 +0200

    fixed form action

--- Frontend/content/login.php
+++ Frontend/content/login.php
@@ -3,10 +3,10 @@
     debug_log("Login as " . $_POST['customer']);
   }
  ?>
-<form method="post">
+<form action="/login" method="post">
  <label for="customer">Kundennummer:</label><br>
  <input type="text" id="customer" name="customer"><br>
  <label for="passwd">Passwort:</label><br>
- <input type="text" id="passwd" name="passwd">
+ <input type="password" id="passwd" name="passwd">
  <input type="submit" value="Login">
 </form>
