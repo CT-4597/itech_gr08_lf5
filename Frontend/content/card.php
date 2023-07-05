808ff3edb732b00ac100fd13589d1dc28e4d848f
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-05 07:45:16 +0200

    warenkorb.... oder so ähnlich

--- Frontend/content/card.php
+++ Frontend/content/card.php
@@ -1,3 +1,8 @@
 <?php
-var_dump($_SESSION['shopping_card_ingredients']);
+if (isset($_SESSION['shopping_card_ingredients'])) {
+    // Schleife über den Warenkorb
+    foreach ($_SESSION['shopping_card_ingredients'] as $ingredient => $amount) {
+        echo "Produkt: " . $ingredient . ", Menge: " . $amount . "<br>";
+    }
+}
  ?>
