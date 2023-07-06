689f86523be274b17fc22188676195c043e3fdff
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 19:36:01 +0200

    changes

--- Frontend/content/order.php
+++ Frontend/content/order.php
@@ -6,15 +6,20 @@ if(isset($_POST['Order'])) {
   $order_id = sql_execute($query_order);
 
   # Order ingredients
-  foreach ($_SESSION['shopping_card_ingredients'] as $ingredient => $amount) {
-    $query_order_ingredient = "INSERT INTO BESTELLUNGZUTAT (BESTELLNR, ZUTATENNR, MENGE) VALUES ($order_id, $ingredient, $amount)";
-    sql_execute($query_order_ingredient);
+  if(count($_SESSION['shopping_card_ingredients']) > 0) {
+    foreach ($_SESSION['shopping_card_ingredients'] as $ingredient => $amount) {
+      $query_order_ingredient = "INSERT INTO BESTELLUNGZUTAT (BESTELLNR, ZUTATENNR, MENGE) VALUES ($order_id, $ingredient, $amount)";
+      sql_execute($query_order_ingredient);
+    }
   }
   # Order boxes
-  foreach ($_SESSION['shopping_card_boxes'] as $box => $amount) {
-    $query_order_box = "INSERT INTO BESTELLUNGSAMMLUNG (BESTELLNR, SAMMLUNGSNR, MENGE) VALUES ($order_id, $box, $amount)";
-    sql_execute($query_order_box);
+  if(count($_SESSION['shopping_card_boxes']) > 0){
+    foreach ($_SESSION['shopping_card_boxes'] as $box => $amount) {
+      $query_order_box = "INSERT INTO BESTELLUNGSAMMLUNG (BESTELLNR, SAMMLUNGSNR, MENGE) VALUES ($order_id, $box, $amount)";
+      sql_execute($query_order_box);
+    }
   }
+
   $_SESSION['shopping_card_ingredients'] = array();
   $_SESSION['shopping_card_boxes'] = array();
 } else {
@@ -24,7 +29,7 @@ if(isset($_POST['Order'])) {
 
 <form action="/" method="post">
   <?php echo $query_order; ?><br>
-  <?php echo $last_id; ?><br>
+  <?php echo $order_id; ?><br>
   Vielen dank für ihre Bestellung.
   <input type="submit" name="back" value="Zurück zur Startseite">
 </form>
