a7cf5c337beafa6ed5cb42a2840cefa2f248bf09
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 07:18:09 +0200

    asd

--- Frontend/content/boxes.php
+++ Frontend/content/boxes.php
@@ -1,6 +1,16 @@
 <?
+  $query = "SELECT BLA FROM BOX ";
+  if($_GET['type'] == 'discount'){
+
+  }
   if($_GET['type'] == 'bio'){
 
   }
-  
+  if($_GET['type'] == 'recipe'){
+
+  }
+
+  # execute sql statement
 ?>
+
+results
