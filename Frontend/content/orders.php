77e2d6e655df76c8e84cea431c754ebfa044efde
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 19:42:24 +0200

    orders

---
+++ Frontend/content/orders.php
@@ -0,0 +1,18 @@
+<?php
+  $user_id = $_SESSION['userid'];
+  $query = "SELECT * FROM BESTELLUNGEN WHERE KUNDENNR=$user_id";
+
+  $result = sql_fetch($query, False);
+?>
+<table>
+<?php
+  if($result != False)
+    while($row = $result->fetch_assoc()) {
+      echo "<tr>";
+      echo "  <td>" . $ROW['BESTELLNR'] . "</td>";
+      echo "  <td>" . $ROW['BESTELLDATUM'] . "</td>";
+      echo "  <td style="text-align: right;">" . $ROW['RECHNUNGSBETRAG'] . " €</td>";
+      echo "</tr>";
+    }
+?>
+</table>
