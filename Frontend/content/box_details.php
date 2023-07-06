aa68fc9a6df41beb7f9135cda2af0cee5879ef53
CT-4597 <stambor97@gmail.com>
2023-07-06 19:11:26 +0200

    max amount box bestellung

--- Frontend/content/box_details.php
+++ Frontend/content/box_details.php
@@ -17,13 +17,14 @@ $sql = log_sql("SELECT SAMMLUNG.SAMMLUNGSNR, SAMMLUNG.SAMMLUNGSBEZEICHNUNG,
                 LEFT JOIN ZUTAT ON ZUTAT.ZUTATENNR = SAMMLUNGZUTAT.ZUTATENNR WHERE SAMMLUNG.SAMMLUNGSNR = " . $_GET['id']);
 $result = $conn->query($sql);
 
-$sql_content = log_sql("SELECT SAMMLUNGZUTAT.ZUTATENNR, ZUTAT.BEZEICHNUNG, SAMMLUNGZUTAT.ZUTATENMENGE FROM SAMMLUNGZUTAT
+$sql_content = log_sql("SELECT SAMMLUNGZUTAT.ZUTATENNR, ZUTAT.BEZEICHNUNG, ZUTAT.BESTAND, SAMMLUNGZUTAT.ZUTATENMENGE FROM SAMMLUNGZUTAT
                         LEFT JOIN ZUTAT ON ZUTAT.ZUTATENNR = SAMMLUNGZUTAT.ZUTATENNR
                         WHERE SAMMLUNGZUTAT.SAMMLUNGSNR = " . $_GET['id']);
 $result_content = $conn->query($sql_content);
 
 if ($result->num_rows > 0) {
 	$row = $result->fetch_assoc();
+    $maxAmount = 1;
 ?>
 <h1 class="details_headline"><?php echo $row['SAMMLUNGSBEZEICHNUNG']; ?></h1>
 <div class="details_container">
@@ -40,6 +41,10 @@ if ($result->num_rows > 0) {
                 echo '<td>' . $row_content['BEZEICHNUNG'] . '</td>';
                 echo '<td>' . $row_content['ZUTATENMENGE'] . '</td>';
                 echo '</tr>';
+
+                if ($maxAmount < ($row_content['BESTAND'] / $row_content['ZUTATENMENGE'])) {
+                    $maxAmount =floor($row_content['BESTAND'] / $row_content['ZUTATENMENGE']);
+                }
             }
             ?>
         </table>
@@ -49,7 +54,7 @@ if ($result->num_rows > 0) {
 
     <div class="details_info">
         <ul>
-            <li>Verfügbar: <?php echo 'TO DO Stück'; ?></li>
+            <li>Verfügbar: <?php echo $maxAmount; ?></li>
             <li>Einzelpreis der Zutaten: <?php echo $row['Gesamtpreis']; ?> €</li>
             <li>Preis in netto: <?php echo $row['RabattPreis']; ?> €</li>
         </ul>
@@ -59,7 +64,7 @@ if ($result->num_rows > 0) {
         <form action="/box/<?php echo $row['SAMMLUNGSNR']; ?>" method="post">
     	    <input type="hidden" name="SAMMLUNGSNR" value="<?php echo $row['SAMMLUNGSNR']; ?>">
             <label for="amount">Menge</label>
-            <input type="number" id="amount" name="amount" min="1" max="10" value="1">
+            <input type="number" id="amount" name="amount" min="1" max="<?php echo $maxAmount; ?>" value="1">
             <input type="submit" name="AddToCard" value="Zum Warenkorb hinzufügen">
         </form>
     </div>
