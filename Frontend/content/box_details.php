3be8ae5e61b5ab66ebbf22489543e1afc8ac4834
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 07:41:20 +0200

    example

---
+++ Frontend/content/box_details.php
@@ -0,0 +1,15 @@
+<?php
+  $id = $_GET['id'];
+
+  $query = "SELECT * FROM KUNDE WHERE KUNDENNR=$id";
+
+
+  # execute sql statement
+  $row = sql_fetch($query);
+
+  $vorname = $row['VORNAME'];
+  $nachname = $row['NACHNAME'];
+
+  echo "$vorname</br>"
+  echo "$nachname</br>"
+?>
