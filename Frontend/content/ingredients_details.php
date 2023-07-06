30c9873b92335ebbf3b86edaf09444b83a921d44
CT-4597 <stambor97@gmail.com>
2023-07-06 16:32:43 +0200

    details styling grid

--- Frontend/content/ingredients_details.php
+++ Frontend/content/ingredients_details.php
@@ -17,32 +17,35 @@ if ($result->num_rows > 0) {
 	$row = $result->fetch_assoc();
 ?>
 <h1 class="details_headline"><?php echo $row['BEZEICHNUNG']; ?></h1>
-<div class="details_nutrition">
-    <h2>Nährwerte</h2>
-    <ul>
-        <li>Kalorien: <?php echo $row['KALORIEN']; ?></li>
-        <li>Kohlenhydrate: <?php echo $row['KOHLENHYDRATE']; ?></li>
-        <li>Protein: <?php echo $row['PROTEIN']; ?></li>
-    </ul>
-</div>
+<div class="details_container">
+    <div class="details_nutrition">
+        <h2>Nährwerte</h2>
+        <ul>
+            <li>Kalorien: <?php echo $row['KALORIEN']; ?></li>
+            <li>Kohlenhydrate: <?php echo $row['KOHLENHYDRATE']; ?></li>
+            <li>Protein: <?php echo $row['PROTEIN']; ?></li>
+        </ul>
+    </div>
 
-<div class="details_info">
-    <ul>
-        <li>Verfügbar: <?php echo $row['BESTAND']; ?> <?php echo $row['EINHEIT']; ?></li>
-        <li>Preis in netto: <?php echo $row['NETTOPREIS']; ?> €</li>
-    </ul>
-</div>
+    <img src="<?php get_image("z", $row['ZUTATENNR']); ?>" alt="Bild der Zutat" class="details_pic">
 
-<div class="details_order">
-    <form action="/zutat/<?php echo $row['ZUTATENNR']; ?>" method="post">
-	    <input type="hidden" name="ZUTATENNR" value="<?php echo $row['ZUTATENNR']; ?>">
-        <label for="amount">Menge</label>
-        <input type="number" id="amount" name="amount" min="1" max="<?php echo $row['BESTAND']; ?>" value="1">
-        <input type="submit" name="AddToCard" value="Zum Warenkorb hinzufügen">
-    </form>
-</div>
+    <div class="details_info">
+        <ul>
+            <li>Verfügbar: <?php echo $row['BESTAND']; ?> <?php echo $row['EINHEIT']; ?></li>
+            <li>Preis in netto: <?php echo $row['NETTOPREIS']; ?> €</li>
+        </ul>
+    </div>
 
-<img src="<?php get_image("z", $row['ZUTATENNR']); ?>" alt="Bild der Zutat" class="details_pic">
+    <div class="details_order">
+        <form action="/zutat/<?php echo $row['ZUTATENNR']; ?>" method="post">
+    	    <input type="hidden" name="ZUTATENNR" value="<?php echo $row['ZUTATENNR']; ?>">
+            <label for="amount">Menge</label>
+            <input type="number" id="amount" name="amount" min="1" max="<?php echo $row['BESTAND']; ?>" value="1">
+            <input type="submit" name="AddToCard" value="Zum Warenkorb hinzufügen">
+        </form>
+    </div>
+
+</div>
 <?php
 } else {
     echo "<h1 class=\"details_headline\">Es wurde keine Zutat gewählt</h1>";
