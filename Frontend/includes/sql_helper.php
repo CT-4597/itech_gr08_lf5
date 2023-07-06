aeead9850a03d4ac2c8ec7fe1b9dd682d6f9b7d5
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 05:29:12 +0200

    comments

--- Frontend/includes/sql_helper.php
+++ Frontend/includes/sql_helper.php
@@ -1,20 +1,26 @@
 <?php
 
   function sql_fetch($query) {
-      $query = log_sql($query);
 
-      $result = $conn->query($query);
+    # Log the query
+    $query = log_sql($query);
 
-      if (!$result) {
-        $message  = 'Invalid query: ' .  $result->error . "</br>";
-        $message .= 'Whole query: ' . highlightKeywords($sql);
-        debug_log($message);
-      }
-      if ($result->num_rows > 0) {
-        return $result->fetch_assoc();
-      } else {
-        return False;
-      }
+    # use conn to get a result
+    $result = $conn->query($query);
+
+    # if there was a error, result is False
+    if (!$result) {
+      $message  = 'Invalid query: ' .  $result->error . "</br>";
+      $message .= 'Whole query: ' . highlightKeywords($sql);
+      debug_log($message);
+      return False;
+    }
+
+    # Return the rows if we got at least one, otherwise return False
+    if ($result->num_rows > 0) {
+      return $result->fetch_assoc();
+    } else {
+      return False;
     }
   }
 
