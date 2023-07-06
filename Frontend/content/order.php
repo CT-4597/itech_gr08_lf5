1de0394810898585f9634af51b2247c8f7dec4d5
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 19:32:18 +0200

    order complete.... maybe

--- Frontend/content/order.php
+++ Frontend/content/order.php
@@ -2,15 +2,28 @@
 if(isset($_POST['Order'])) {
   $userid = $_SESSION['userid'];
   $price_total = (float)$_POST['price_total'];
-  $query = "INSERT INTO BESTELLUNG (KUNDENNR, BESTELLDATUM, RECHNUNGSBETRAG) VALUES ($userid, now(), $price_total)";
-  $last_id = sql_execute($query);
+  $query_order = "INSERT INTO BESTELLUNG (KUNDENNR, BESTELLDATUM, RECHNUNGSBETRAG) VALUES ($userid, now(), $price_total)";
+  $order_id = sql_execute($query_order);
+
+  # Order ingredients
+  foreach ($_SESSION['shopping_card_ingredients'] as $ingredient => $amount) {
+    $query_order_ingredient = "INSERT INTO BESTELLUNGZUTAT (BESTELLNR, ZUTATENNR, MENGE) VALUES ($order_id, $ingredient, $amount)";
+    sql_execute($query_order_ingredient);
+  }
+  # Order boxes
+  foreach ($_SESSION['shopping_card_boxes'] as $box => $amount) {
+    $query_order_box = "INSERT INTO BESTELLUNGSAMMLUNG (BESTELLNR, SAMMLUNGSNR, MENGE) VALUES ($order_id, $box, $amount)";
+    sql_execute($query_order_box);
+  }
+  $_SESSION['shopping_card_ingredients'] = array();
+  $_SESSION['shopping_card_boxes'] = array();
 } else {
   header("Location: /");
 }
 ?>
 
 <form action="/" method="post">
-  <?php echo $query; ?><br>
+  <?php echo $query_order; ?><br>
   <?php echo $last_id; ?><br>
   Vielen dank für ihre Bestellung.
   <input type="submit" name="back" value="Zurück zur Startseite">
