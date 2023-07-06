d1d3e995914911fcb7fada372b048173ec26dfa5
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 07:26:32 +0200

    something

--- Frontend/content/boxes.php
+++ Frontend/content/boxes.php
@@ -1,5 +1,9 @@
 <?
-  $query = "SELECT BLA FROM BOX ";
+  $query = "SELECT * FROM SAMMLUNG
+			LEFT JOIN (SELECT SAMMLUNGZUTAT.SAMMLUNGSNR AS SAMMLUNGMITALLERGENNR FROM SAMMLUNGZUTAT JOIN ZUTATALLERGEN ON SAMMLUNGZUTAT.ZUTATENNR = ZUTATALLERGEN.ZUTATENNR WHERE ZUTATALLERGEN.ALLERGENNR = 1) sub
+            ON SAMMLUNG.SAMMLUNGSNR = sub.SAMMLUNGMITALLERGENNR
+            WHERE SAMMLUNGMITALLERGENNR IS NULL";
+
   if($_GET['type'] == 'discount'){
 
   }
@@ -11,6 +15,9 @@
   }
 
   # execute sql statement
-?>
+  $result = sql_fetch($query);
 
-results
+  while($row = $result->fetch_assoc()) {
+    echo "dataset";
+  }
+?>
