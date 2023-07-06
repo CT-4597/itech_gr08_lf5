732cab53512ff64818abf311641931d388df41c1
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 21:32:05 +0200

    kaput

--- Frontend/extras/dsgvodump.php
+++ Frontend/extras/dsgvodump.php
@@ -3,32 +3,40 @@
 
   session_start();
 
+  function getcsv($query) {
+    $result = $conn->query($query_dsgvo_download);
+
+    // get attribute names
+    $fields = $result->fetch_fields();
+    $column_names = array();
+    foreach ($fields as $field) {
+        $column_names[] = $field->name;
+    }
+
+    // create csv
+    $csv_data = implode(",", $column_names) . "\n";
+    while ($row = $result->fetch_assoc()) {
+        $csv_data .= implode(",", $row) . "\n";
+    }
+    return $csv_data;
+  }
   $output_filename = "dsgvodump_" . (string)$_SESSION['userid'] . ".csv";
 
   $user_id = $_SESSION['userid'];
+
+  $query_personal_data = "SELECT * FROM KUNDE WHERE KUNDENNR=$user_id";
+
   $query_dsgvo_download = "SELECT KUNDE.*, BESTELLUNG.*, SAMMLUNG.SAMMLUNGSBEZEICHNUNG FROM KUNDE LEFT JOIN BESTELLUNG ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR LEFT JOIN BESTELLUNGSAMMLUNG ON BESTELLUNGSAMMLUNG.BESTELLNR = BESTELLUNG.BESTELLNR LEFT JOIN SAMMLUNG ON SAMMLUNG.SAMMLUNGSNR = BESTELLUNGSAMMLUNG.SAMMLUNGSNR WHERE KUNDE.KUNDENNR = $user_id";
 
-  $result = $conn->query($query_dsgvo_download);
-
-  // get attribute names
-  $fields = $result->fetch_fields();
-  $column_names = array();
-  foreach ($fields as $field) {
-      $column_names[] = $field->name;
-  }
-
-  // create csv
-  $csv_data = implode(",", $column_names) . "\n";
-  while ($row = $result->fetch_assoc()) {
-      $csv_data .= implode(",", $row) . "\n";
-  }
+  $csv_data = '';
+  $csv_data .= getcsv($query_personal_data);
 
   // Dateidownload-Header setzen
   # header("Content-Type: text/csv");
   # header("Content-Disposition: attachment; filename=$output_filename");
 
   // CSV-Daten an den Browser senden
-  echo $query_dsgvo_download;
+  echo $csv_data;
   echo "<br>";
   echo "<br>";
   echo $csv_data;
