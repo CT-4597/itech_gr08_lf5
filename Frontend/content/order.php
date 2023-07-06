2034c0f19c52d9a48ed36780770810c6f8eeb1c7
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 19:22:09 +0200

    bestellung

--- Frontend/content/order.php
+++ Frontend/content/order.php
@@ -1,12 +1,15 @@
 <?php
 if(isset($_POST['Order'])) {
-
+  $userid = $_SESSION['userid'];
+  $price_total = (float)$_POST['price_total'];
+  $query = "INSERT INTO BESTELLUNG (KUNDENNR, BESTELLDATUM, RECHNUNGSBETRAG) VALUES ($userid, now(), $price_total)";
 } else {
   header("Location: /");
 }
 ?>
 
 <form action="/" method="post">
+  <?php echo $query; ?>
   Vielen dank für ihre Bestellung.
   <input type="submit" name="back" value="Zurück zur Startseite">
 </form>
