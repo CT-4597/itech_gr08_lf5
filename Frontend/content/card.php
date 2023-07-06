084f8c4317b79357d42b946238228fa474f26b42
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 16:26:57 +0200

    test

--- Frontend/content/card.php
+++ Frontend/content/card.php
@@ -1,10 +1,16 @@
 <form>
 <?php
-if (isset($_SESSION['shopping_card_ingredients'])) {
+# $result = sql_fetch();
+if (isset($_SESSION['shopping_card_ingredients']) && count($_SESSION['shopping_card_ingredients']) > 0) {
     // Schleife über den Warenkorb
+    $query = "SELECT BEZEICHNUNG FROM ZUTAT WHERE";
+    $first = True;
     foreach ($_SESSION['shopping_card_ingredients'] as $ingredient => $amount) {
-        echo "Produkt: " . $ingredient . ", Menge: " . $amount . "<br>";
+      echo "Produkt: " . $ingredient . ", Menge: " . $amount . "<br>";
+      $query .= ($first ? '': ' OR ') . 'ZUTATENNR=$ingredient';
+      $first = False;
     }
+    echo $query;
 }
  ?>
  <?php
