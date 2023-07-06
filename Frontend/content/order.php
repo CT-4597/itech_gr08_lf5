67462517577472889ac02397e7cddb63e734aa88
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-06 19:25:39 +0200

    asd

--- Frontend/content/order.php
+++ Frontend/content/order.php
@@ -3,13 +3,15 @@ if(isset($_POST['Order'])) {
   $userid = $_SESSION['userid'];
   $price_total = (float)$_POST['price_total'];
   $query = "INSERT INTO BESTELLUNG (KUNDENNR, BESTELLDATUM, RECHNUNGSBETRAG) VALUES ($userid, now(), $price_total)";
+  $last_id = sql_execute($query);
 } else {
   header("Location: /");
 }
 ?>
 
 <form action="/" method="post">
-  <?php echo $query; ?>
+  <?php echo $query; ?><br>
+  <?php echo $last_id; ?><br>
   Vielen dank für ihre Bestellung.
   <input type="submit" name="back" value="Zurück zur Startseite">
 </form>
