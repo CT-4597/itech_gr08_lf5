88c38f31aaa278afac7a37778adba84c5c5472f8
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-08 11:00:26 +0200

    test

--- Frontend/controllers/filters.php
+++ Frontend/controllers/filters.php
@@ -73,7 +73,7 @@ class ControllerFilters {
     }
 
     public function RunLate() {
-        Logger::log("Filter Allergies:" . implode(" ", $_SESSION['allergies']));
+        # Logger::log("Filter Allergies:" . implode(" ", $_SESSION['allergies']));
         Logger::log("Filter Category: {$_SESSION['category']}");
     }
 }
