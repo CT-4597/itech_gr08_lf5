53c5bd302cac807936516344c96799120c0cd7ed
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-08 08:01:03 +0200

    asd

--- Frontend/controllers/template.php
+++ Frontend/controllers/template.php
@@ -28,9 +28,11 @@ class ControllerTemplate {
 
     public function early() {
         global $controller_vars;
+        $controller_vars['ingredients'] = $this->db->executeQuery($query, $params);
         $query = "SELECT * FROM ZUTAT WHERE ZUTATENNR=:id";
         $params = array(':id' => 1001);
-        $controller_vars['ingredients'] = $this->db->executeQuery($query, $params);
+        $controller_vars += ['ingredients' => $this->db->executeQuery($query, $params)]
+        # $controller_vars['ingredients'] = $this->db->executeQuery($query, $params);
     }
 
     public function late() {
