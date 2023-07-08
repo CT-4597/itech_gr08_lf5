181e842d7143b84698bd27a946ca95bb72dc9b09
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-08 13:28:56 +0200

    ingredient links added

--- Frontend/views/ingredients.php
+++ Frontend/views/ingredients.php
@@ -6,7 +6,7 @@
 <?php foreach($vars['ingredients'] as $ingredient) { ?>
     <tr>
         <td><?php echo $ingredient['ZUTATENNR']; ?></td>
-        <td><?php echo $ingredient['BEZEICHNUNG']; ?></td>
+        <td><a href="zutat/<?php echo $ingredient['ZUTATENNR']; ?>"><?php echo $ingredient['BEZEICHNUNG']; ?></a></td>
     </tr>
  <?php } ?>
  </table>
