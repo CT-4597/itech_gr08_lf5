7ec4a2d0f9c73c635d7ce59417add37117b1c0f7
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-08 13:00:37 +0200

    ingredient filtering

--- Frontend/controllers/ingredients.php
+++ Frontend/controllers/ingredients.php
@@ -29,10 +29,34 @@ class ControllerIngredients {
 
     public function RunDefault() {
         global $vars;
+        $query = "SELECT DISTINCT ZUTAT.ZUTATENNR, ZUTAT.BEZEICHNUNG, ZUTAT.NETTOPREIS FROM ZUTAT
+            LEFT JOIN ZUTATALLERGEN ON ZUTATALLERGEN.ZUTATENNR = ZUTAT.ZUTATENNR
+            LEFT JOIN ALLERGEN ON ALLERGEN.ALLERGENNR = ZUTATALLERGEN.ALLERGENNR
+            LEFT JOIN ZUTATKATEGORIE ON ZUTATKATEGORIE.ZUTATENNR = ZUTAT.ZUTATENNR
+            LEFT JOIN ERNAEHRUNGSKATEGORIE ON ERNAEHRUNGSKATEGORIE.KATEGORIENR = ZUTATKATEGORIE.KATEGORIENR
+            WHERE ((ZUTATALLERGEN.ALLERGENNR != 0)" . ")"; # Pulsar highlighting is shit with it.... so i need to add a ) and remove it in the next line
+        $query = substr($query, 0, -1); # Still the workaround for the hightlighting
+
+        $params = array();
+        $placeholders = array();
+
+        foreach ($_SESSION['allergies'] as $id) {
+            $placeholders[] = 'ZUTATALLERGEN.ALLERGENNR != :allergen_' . $id;
+            $params[':allergen_' . $id] = $id;
+        }
+
+        $sql .= ' AND (' . implode(' AND ', $placeholders) . ')';
+
+        $sql .= ' OR ZUTATALLERGEN.ALLERGENNR IS NULL)';
+
+        if ($_SESSION['categories'] != NULL && $_SESSION['categories'] != "0") {
+            $sql .= ' AND ERNAEHRUNGSKATEGORIE.KATEGORIENR = :category';
+            $params[':category'] = $_SESSION['categories'];
+        }
 
         # DB Request with params
-        $query = "SELECT * FROM ZUTAT WHERE EINHEIT=:unit";
-        $params = array(':unit' => 'Stück');
+        # $query = "SELECT * FROM ZUTAT WHERE EINHEIT=:unit";
+        # $params = array(':unit' => 'Stück');
         $vars['ingredients'] = $this->db->executeQuery($query, $params);
     }
 
