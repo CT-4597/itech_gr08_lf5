6fa9d55ccf5bdbb172082527e1e104e154270998
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-08 08:58:54 +0200

    removed old queries

--- Frontend/controllers/ingredients.php
+++ Frontend/controllers/ingredients.php
@@ -30,20 +30,10 @@ class ControllerIngredients {
     public function RunDefault() {
         global $vars;
 
-        # DB Request without params
-        /*$query = "SELECT * FROM ZUTAT";
-        $vars['ingredients'] = $this->db->executeQuery($query, array());*/
-
         # DB Request with params
-        /*$query = "SELECT * FROM ZUTAT WHERE EINHEIT=:unit";
+        $query = "SELECT * FROM ZUTAT WHERE EINHEIT=:unit";
         $params = array(':unit' => 'Stück');
-        $vars['ingredients'] = $this->db->executeQuery($query, $params);*/
-
-        # DB Request with only a single row expected
-        # Take a look at controllers/header_user.php for a better example
-        $query = "SELECT * FROM ZUTAT WHERE ZUTATENNR=:id";
-        $params = array(':id' => 1001);
-        $vars['ingredients'][] = $this->db->executeSingleRowQuery($query, $params);
+        $vars['ingredients'] = $this->db->executeQuery($query, $params);
     }
 
     public function RunLate() {
