134e5c3ee43c3a017f26dc3d7e6ea42aa4070e99
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-08 07:14:37 +0200

    public

--- Frontend/controller/header_user.php
+++ Frontend/controller/header_user.php
@@ -2,16 +2,16 @@
 
 class ControllerHeaderUser {
     # Do you need to be loggin for this controller?
-    private bool $logged_in = False;
-    private string $logged_in_redirect = '/';
+    public bool $logged_in = False;
+    public string $logged_in_redirect = '/';
     # Do you need to be a admin?
-    private bool $admin = False;
-    private string $admin_redirect = '/';
+    public bool $admin = False;
+    public string $admin_redirect = '/';
 
     # The viewer name. without suffix.
-    private string $viewer = 'header_user';
+    public string $viewer = 'header_user';
     # the container the viewer is rendered in.
-    private string $container = 'HeaderUser';
+    public string $container = 'HeaderUser';
 
     public function __construct(array &$arr, &$db) {
         array_push($arr, $this);
--- Frontend/controller/main.php
+++ Frontend/controller/main.php
@@ -2,16 +2,16 @@
 
 class ControllerMain {
     # Do you need to be loggin for this controller?
-    private bool $logged_in = False;
-    private string $logged_in_redirect = '/';
+    public bool $logged_in = False;
+    public string $logged_in_redirect = '/';
     # Do you need to be a admin?
-    private bool $admin = False;
-    private string $admin_redirect = '/';
+    public bool $admin = False;
+    public string $admin_redirect = '/';
 
     # The viewer name. without suffix.
-    private string $viewer = 'main';
+    public string $viewer = 'main';
     # the container the viewer is rendered in.
-    private string $container = 'Content';
+    public string $container = 'Content';
 
     public function __construct(array &$arr, &$db) {
         array_push($arr, $this);
