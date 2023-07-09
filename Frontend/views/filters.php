936389e67c504bc46efcfc5e3a693ae3e8fa7e90
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 08:48:41 +0200

    fixed filter element names

--- Frontend/views/filters.php
+++ Frontend/views/filters.php
@@ -1,15 +1,15 @@
 <form method="post">
     <h3>Allergene</h3>
     <?php foreach($vars['allergies'] as $allergy) { ?>
-        <label for="<?php echo  $allergy['ALLERGENNR']; ?>"><?php echo  $allergy['ALLERGENBEZEICHNUNG']; ?></label>
-        <input type="checkbox" id="<?php echo  $allergy['ALLERGENNR']; ?>" name="allergies[]" value="<?php echo  $allergy['ALLERGENNR']; ?>"<?php  echo in_array((string)$allergy['ALLERGENNR'], $_SESSION['allergies']) ? ' checked':''; ?>>
+        <label for="allergy<?php echo  $allergy['ALLERGENNR']; ?>"><?php echo  $allergy['ALLERGENBEZEICHNUNG']; ?></label>
+        <input type="checkbox" id="allergy<?php echo  $allergy['ALLERGENNR']; ?>" name="allergies[]" value="<?php echo  $allergy['ALLERGENNR']; ?>"<?php  echo in_array((string)$allergy['ALLERGENNR'], $_SESSION['allergies']) ? ' checked':''; ?>>
     <?php }?>
     <h3>Kategorien</h3>
-    <label for="all">Alle</label>
-    <input type="radio" id="all" name="category" value="NULL"<?php echo ($_SESSION['category'] == NULL) ? ' checked' : '';?>>
+    <label for="categoryall">Alle</label>
+    <input type="radio" id="categoryall" name="category" value="NULL"<?php echo ($_SESSION['category'] == NULL) ? ' checked' : '';?>>
     <?php foreach($vars['nutrition_categories'] as $category) { ?>
-        <label for="<?php echo  $category['KATEGORIENR']; ?>"><?php echo  $category['KATEGORIEBEZEICHNUNG']; ?></label>
-        <input type="radio" id="<?php echo  $category['KATEGORIENR']; ?>" name="category" value="<?php echo  $category['KATEGORIENR']; ?>"<?php echo ($category['KATEGORIENR'] == $_SESSION['category']) ? ' checked' : ''; ?>>
+        <label for="category<?php echo  $category['KATEGORIENR']; ?>"><?php echo  $category['KATEGORIEBEZEICHNUNG']; ?></label>
+        <input type="radio" id="category<?php echo  $category['KATEGORIENR']; ?>" name="category" value="<?php echo  $category['KATEGORIENR']; ?>"<?php echo ($category['KATEGORIENR'] == $_SESSION['category']) ? ' checked' : ''; ?>>
     <?php }?>
 
 <input type="submit" name="FiltersApply" value="Übernehmen">
