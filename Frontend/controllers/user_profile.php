a37ec2f956794ee9f6c6083b014f445e4052cc83
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 10:13:26 +0200

    user profile

---
+++ Frontend/controllers/user_profile.php
@@ -0,0 +1,17 @@
+<?php
+# Needs to be the same as class [NAME]
+new ControllerUserProfile($controllers, $db, ["Content" => "user_profile"]);
+# Be sure to give it a unique name.
+class ControllerTemplate extends BaseController {
+    public function RunDefault() {
+        global $vars;
+        # DB Request with only a single row expected
+        # Take a look at controllers/header_user.php for a better example
+        $query = "SELECT * FROM KUNDE WHERE SESSIONID=:sessionid";
+        $params = array(':sessionid' => session_id());
+        $vars['user_profile'][] = $this->db->executeSingleRowQuery($query, $params);
+    }
+}
+
+
+?>
