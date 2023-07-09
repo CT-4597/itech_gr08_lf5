d73d2a25f953d5a9dd6c2b4b86e20fe919aefad5
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 10:21:39 +0200

    user header

--- Frontend/controllers/header_user.php
+++ Frontend/controllers/header_user.php
@@ -3,6 +3,17 @@
 new ControllerHeaderUser($controllers, $db, ["HeaderUser" => "header_user"]);
 # Be sure to give it a unique name.
 class ControllerHeaderUser extends BaseController {
+    public function RunDefault() {
+        global $vars;
+        global $auth;
 
+        if($auth->logged_in()) {
+            # DB Request with only a single row expected
+            # Take a look at controllers/header_user.php for a better example
+            $query = "SELECT VORNAME, NACHNAME FROM KUNDE WHERE SESSIONID=:sessionid";
+            $params = array(':sessionid' => session_id());
+            $vars['user_header'][] = $this->db->executeSingleRowQuery($query, $params);
+        }
+    }
 }
 ?>
--- Frontend/views/header_user.php
+++ Frontend/views/header_user.php
@@ -1,2 +1,9 @@
-<a href="/login">Login</a>
-<a href="/registrieren">Register</a>
+<?php if($auth->logged_in()) { ?>
+    Hallo <?php echo $vars['user_header']['VORNAME']; ?> <?php echo $vars['user_header']['NACHNAME']; ?> <br>
+    <a href="/profil">Profile</a> &nbsp;
+    <a href="/bestellungen">Bestellungen</a> &nbsp;
+    <a href="/logout">Logout</a>
+<?php } else { ?>
+    <a href="/login">Login</a>
+    <a href="/registrieren">Register</a>
+<?php } ?>
