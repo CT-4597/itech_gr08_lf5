ae89700cf7a14357ebe0dbbb36a8bcd7d4f1e1e8
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 09:34:22 +0200

    test

--- Frontend/views/filters.php
+++ Frontend/views/filters.php
@@ -1,15 +1,15 @@
 <form method="post">
     <h3>Allergene</h3>
     <?php foreach($vars['allergies'] as $allergy) { ?>
-        <label class="FilterLabel" for="allergy<?php echo  $allergy['ALLERGENNR']; ?>"><?php echo  $allergy['ALLERGENBEZEICHNUNG']; ?></label>
-        <input type="checkbox" id="allergy<?php echo  $allergy['ALLERGENNR']; ?>" name="allergies[]" value="<?php echo  $allergy['ALLERGENNR']; ?>"<?php  echo in_array((string)$allergy['ALLERGENNR'], $_SESSION['allergies']) ? ' checked':''; ?>>
+    <input type="checkbox" id="allergy<?php echo  $allergy['ALLERGENNR']; ?>" name="allergies[]" value="<?php echo  $allergy['ALLERGENNR']; ?>"<?php  echo in_array((string)$allergy['ALLERGENNR'], $_SESSION['allergies']) ? ' checked':''; ?>>
+    <label class="FilterLabel" for="allergy<?php echo  $allergy['ALLERGENNR']; ?>"><?php echo  $allergy['ALLERGENBEZEICHNUNG']; ?></label>
     <?php }?>
     <h3>Kategorien</h3>
-    <label class="FilterLabel"  for="categoryall">Alle</label>
     <input type="radio" id="categoryall" name="category" value="NULL"<?php echo ($_SESSION['category'] == NULL) ? ' checked' : '';?>>
+    <label class="FilterLabel"  for="categoryall">Alle</label>
     <?php foreach($vars['nutrition_categories'] as $category) { ?>
-        <label class="FilterLabel"  for="category<?php echo  $category['KATEGORIENR']; ?>"><?php echo  $category['KATEGORIEBEZEICHNUNG']; ?></label>
         <input type="radio" id="category<?php echo  $category['KATEGORIENR']; ?>" name="category" value="<?php echo  $category['KATEGORIENR']; ?>"<?php echo ($category['KATEGORIENR'] == $_SESSION['category']) ? ' checked' : ''; ?>>
+        <label class="FilterLabel"  for="category<?php echo  $category['KATEGORIENR']; ?>"><?php echo  $category['KATEGORIEBEZEICHNUNG']; ?></label>
     <?php }?>
 
 <input type="submit" name="FiltersApply" value="Übernehmen">
