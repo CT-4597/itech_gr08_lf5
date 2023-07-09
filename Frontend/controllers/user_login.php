83654c897b964168a87479d1103ff1dda70a75d5
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 09:47:40 +0200

    login test

--- Frontend/controllers/user_login.php
+++ Frontend/controllers/user_login.php
@@ -3,8 +3,22 @@
 new ControllerUserLogin($controllers, $db, ["Content" => "user_login"]);
 # Be sure to give it a unique name.
 class ControllerUserLogin extends BaseController {
+    public function RunDefault() {
+        global $vars;
 
+        # TODO Add Checks for fields
+        # TODO Check if EMAILS already exists
+
+        # DB Request with only a single row expected
+        # Take a look at controllers/header_user.php for a better example
+        if(isset($_POST['LoginUser'])) {
+            $query = "SELECT id FROM KUNDE WHERE EMAIL=:email AND PASSWORT=:password";
+            $params = array(':email' => $_GET['email'],
+                            ':password' => $_GET['passwd']);
+            $vars['userlogin'] = $this->db->executeSingleRowQuery($query, $params);
+            Logger::log('User ID: ' . $vars['userlogin']['KUNDENNR']);
+        }
+    }
 }
-
 
 ?>
--- Frontend/views/user_login.php
+++ Frontend/views/user_login.php
@@ -2,9 +2,9 @@
     <form action="/login" method="post"  class="loginform">
         <h1>Login</h1>
         <label for="email">E-Mail:</label><br>
-        <input type="text" id="email" name="email"><br>
+        <input type="email" id="email" name="email"><br>
         <label for="passwd">Passwort:</label><br>
         <input type="password" id="passwd" name="passwd">
-        <input type="submit" name="Login" value="Login">
+        <input type="submit" name="LoginUser" value="Login">
     </form>
 </div>
