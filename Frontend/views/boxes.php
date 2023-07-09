49d5f018b24d9f3f8174cd295544956613dc8095
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 06:14:57 +0200

    fixed link

--- Frontend/views/boxes.php
+++ Frontend/views/boxes.php
@@ -6,7 +6,7 @@
 <?php foreach($vars['boxes'] as $box) { ?>
     <tr>
         <td><?php echo $box['SAMMLUNGSNR']; ?></td>
-        <td><a href="zutat/<?php echo $box['SAMMLUNGSNR']; ?>"><?php echo $box['SAMMLUNGSBEZEICHNUNG']; ?></a></td>
+        <td><a href="/box/<?php echo $box['SAMMLUNGSNR']; ?>"><?php echo $box['SAMMLUNGSBEZEICHNUNG']; ?></a></td>
     </tr>
  <?php } ?>
  </table>
