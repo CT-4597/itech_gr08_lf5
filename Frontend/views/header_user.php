6f1185c8bac40f4b5d9fdde3902f142b9d96385e
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 12:21:44 +0200

    undo

--- Frontend/views/header_user.php
+++ Frontend/views/header_user.php
@@ -1,13 +1,13 @@
 <?php if($auth->LoggedIn()) { ?>
     Hallo <?php echo $vars['user_header']['VORNAME']; ?> <?php echo $vars['user_header']['NACHNAME']; ?> <br>
      <br>
-    <a href="/profil">Profile</a> <br>
-    <a href="/bestellungen">Bestellungen</a> <br>;
+    <a href="/profil">Profile</a> &nbsp;
+    <a href="/bestellungen">Bestellungen</a> &nbsp;
     <?php if($auth->Admin()) { ?>
-        <a href="/admin/">Administration</a>  <br>
+        <a href="/admin/">Administration</a>  &nbsp;
     <?php } ?>
-    <a href="/logout">Logout</a>
+    <a href="/logout">Logout</a>  &nbsp;
 <?php } else { ?>
-    <a href="/login">Login</a> <br>
+    <a href="/login">Login</a>  &nbsp;
     <a href="/registrieren">Register</a>
 <?php } ?>
