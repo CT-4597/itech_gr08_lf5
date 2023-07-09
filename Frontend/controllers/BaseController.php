cab3940709282600389b6cce251b4a521451443c
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 06:35:16 +0200

    BaseController Class

---
+++ Frontend/controllers/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+class BaseController {
+    protected bool $logged_in;
+    protected string $logged_in_redirect;
+    protected bool $admin;
+    protected string $admin_redirect;
+    protected string $view;
+    protected string $container;
+    protected $db;
+
+    public function __construct(array &$arr, &$db, string $view = 'template', string $container = 'Content', bool $logged_in = false, string $logged_in_redirect = '/', bool $admin = false, string $admin_redirect = '/') {
+        array_push($arr, $this);
+        $this->db = $db;
+        $this->logged_in = $logged_in;
+        $this->logged_in_redirect = $logged_in_redirect;
+        $this->admin = $admin;
+        $this->admin_redirect = $admin_redirect;
+        $this->view = $view;
+        $this->container = $container;
+    }
+
+    public function runEarly() {
+        // Standardimplementierung oder leer lassen
+    }
+
+    public function runDefault() {
+        global $vars;
+        // Standardimplementierung oder leer lassen
+    }
+
+    public function runLate() {
+        // Standardimplementierung oder leer lassen
+    }
+}
+?>
--- Frontend/controllers/template.php
+++ Frontend/controllers/template.php
@@ -1,32 +1,7 @@
 <?php
 
 # Be sure to give it a unique name.
-class ControllerTemplate {
-    # Do you need to be loggin for this controller?
-    public bool $logged_in = False;
-    public string $logged_in_redirect = '/';
-    # Do you need to be a admin?
-    public bool $admin = False;
-    public string $admin_redirect = '/';
-
-    # The viewer name. without suffix.
-    # views can be reused if build for
-    public string $view = 'template';
-    # the container the viewer is rendered in.
-    public string $container = 'Content';
-
-    # var holden the database class
-    public $db;
-
-    public function __construct(array &$arr, &$db) {
-        array_push($arr, $this);
-        $this->db = $db;
-    }
-
-    public function RunEarly() {
-
-    }
-
+class ControllerTemplate extends BaseController {
     public function RunDefault() {
         global $vars;
 
@@ -45,12 +20,8 @@ class ControllerTemplate {
         $params = array(':id' => 1001);
         $vars['ingredients'][] = $this->db->executeSingleRowQuery($query, $params);
     }
-
-    public function RunLate() {
-        
-    }
 }
 
 # Needs to be the same as class [NAME]
-new ControllerTemplate($controllers, $db);
+new ControllerTemplate($controllers, $db, 'template', 'Content');
 ?>
