ccae4fd024ce0ff012a7a8f7d7fe82169d913665
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 11:52:01 +0200

    test

--- Frontend/controllers/boxes_details.php
+++ Frontend/controllers/boxes_details.php
@@ -9,6 +9,7 @@ class ControllerBoxesDetails extends BaseController {
         # DB Request with only a single row expected
         # Take a look at controllers/header_user.php for a better example
 
+        # Get Box Details
         $query = "SELECT SAMMLUNG.SAMMLUNGSNR, SAMMLUNG.SAMMLUNGSBEZEICHNUNG,
                         sum(ZUTAT.NETTOPREIS * SAMMLUNGZUTAT.ZUTATENMENGE) AS 'Gesamtpreis',
                         sum(ZUTAT.NETTOPREIS * SAMMLUNGZUTAT.ZUTATENMENGE)-round(sum(ZUTAT.NETTOPREIS * SAMMLUNGZUTAT.ZUTATENMENGE)*(SAMMLUNG.RABATT/100),2)
@@ -18,13 +19,14 @@ class ControllerBoxesDetails extends BaseController {
         $params = array(':id' => $_GET['id']);
         $vars['box'] = $this->db->executeSingleRowQuery($query, $params);
 
+        # Get Box Ingredients
         $query = "SELECT SAMMLUNGZUTAT.ZUTATENNR, ZUTAT.BEZEICHNUNG, ZUTAT.BESTAND, SAMMLUNGZUTAT.ZUTATENMENGE FROM SAMMLUNGZUTAT
                                 LEFT JOIN ZUTAT ON ZUTAT.ZUTATENNR = SAMMLUNGZUTAT.ZUTATENNR
                                 WHERE SAMMLUNGZUTAT.SAMMLUNGSNR=:id";
         $params = array(':id' => $_GET['id']);
         $vars['box_content'] = $this->db->executeQuery($query, $params);
 
-        floor($content['BESTAND'] / $content['ZUTATENMENGE'])
+        # Get Max Possible Box amount depending on ingredients
         $vars['box']['maxAmount'] = NULL;
         foreach($vars['box_content'] as $content)
             if(is_null($vars['box']['maxAmount']) || ($vars['box']['maxAmount'] > $content['BESTAND'] / $content['ZUTATENMENGE']))
