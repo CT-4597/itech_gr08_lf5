1a277c43929a72ede07f1518101b547e5c3e4482
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-09 05:47:52 +0200

    boxes initial commit

---
+++ Frontend/controllers/boxes.php
@@ -0,0 +1,67 @@
+<?php
+
+# Be sure to give it a unique name.
+class ControllerBoxes {
+    # Do you need to be loggin for this controller?
+    public bool $logged_in = False;
+    public string $logged_in_redirect = '/';
+    # Do you need to be a admin?
+    public bool $admin = False;
+    public string $admin_redirect = '/';
+
+    # The viewer name. without suffix.
+    # views can be reused if build for
+    public string $view = 'boxes';
+    # the container the viewer is rendered in.
+    public string $container = 'Content';
+
+    # var holden the database class
+    public $db;
+
+    public function __construct(array &$arr, &$db) {
+        array_push($arr, $this);
+        $this->db = $db;
+    }
+
+    public function RunEarly() {
+
+    }
+
+    public function RunDefault() {
+        global $vars;
+
+        # adding allergies to clause
+        $params = array();
+        $placeholders = array();
+
+        foreach ($_SESSION['allergies'] as $id) {
+            $placeholders[] = 'ZUTATALLERGEN.ALLERGENNR != :allergen_' . $id;
+            $params[':allergen_' . $id] = $id;
+        }
+        if(count($_SESSION['allergies']) > 0)
+            $query_allergies .= implode(' OR ', $placeholders);
+
+        # Adding category clause placeholder to query
+        if ($_SESSION['category'] != NULL && $_SESSION['category'] != "0") {
+            $query_catergory .= ' AND ERNAEHRUNGSKATEGORIE.KATEGORIENR = :category';
+            $params[':category'] = $_SESSION['category'];
+        }
+
+        # query with allergies and category
+        $query = "SELECT * FROM SAMMLUNG
+                    LEFT JOIN (SELECT SAMMLUNGZUTAT.SAMMLUNGSNR AS SAMMLUNGMITALLERGENNR FROM SAMMLUNGZUTAT JOIN ZUTATALLERGEN
+                    ON SAMMLUNGZUTAT.ZUTATENNR = ZUTATALLERGEN.ZUTATENNR WHERE FALSE $query_catergory) sub
+                    ON SAMMLUNG.SAMMLUNGSNR = sub.SAMMLUNGMITALLERGENNR
+                    WHERE SAMMLUNGMITALLERGENNR IS NULL$query_catergory";
+
+        $vars['boxes'] = $this->db->executeQuery($query, $params);
+    }
+
+    public function RunLate() {
+
+    }
+}
+
+# Needs to be the same as class [NAME]
+new ControllerBoxes($controllers, $db);
+?>
---
+++ Frontend/views/boxes.php
@@ -0,0 +1,12 @@
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+    </tr>
+<?php foreach($vars['boxes'] as $box) { ?>
+    <tr>
+        <td><?php echo $box['SAMMLUNGSNR']; ?></td>
+        <td><a href="zutat/<?php echo $box['SAMMLUNGSNR']; ?>"><?php echo $box['SAMMLUNGSBEZEICHNUNG']; ?></a></td>
+    </tr>
+ <?php } ?>
+ </table>
