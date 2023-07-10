e4403c5172c7655366259ab0bc3622cdbce2ca2b
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:52:07 +0200

    ids removed

--- Frontend/views/user_order_details.php
+++ Frontend/views/user_order_details.php
@@ -15,7 +15,6 @@
 
     <?php foreach($vars['order_ingredients'] as $ingredient) { ?>
         <tr onclick="window.location.href = '/zutat/<?php echo $ingredient['ZUTATENNR']; ?>'">
-            <td><?php echo $ingredient['ZUTATENNR']; ?></td>
             <td><?php echo $ingredient['BEZEICHNUNG']; ?></td>
             <td><?php echo $ingredient['MENGE']; ?></td>
             <td><?php echo $ingredient['EINHEIT']; ?></td>
@@ -29,7 +28,6 @@
 
     <?php foreach($vars['order_boxes'] as $box) { ?>
         <tr onclick="window.location.href = '/box/<?php echo $box['SAMMLUNGSNR']; ?>'">
-            <td><?php echo $box['SAMMLUNGSNR']; ?></td>
             <td><?php echo $box['SAMMLUNGSBEZEICHNUNG']; ?></td>
             <td><?php echo $box['MENGE']; ?></td>
             <td><?php echo $box['EINZELPREIS']; ?></td>
