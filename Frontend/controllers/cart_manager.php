a1b2562abd97044a4649f93d6e32a0dc1adafc76
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 07:37:12 +0200

    moved method

--- Frontend/controllers/cart_manager.php
+++ Frontend/controllers/cart_manager.php
@@ -5,44 +5,6 @@ new ControllerCartManager($controllers, $db, ["CartDisplay" => 'cart_display']);
 # Be sure to give it a unique name.
 class ControllerCartManager extends BaseController {
 
-    public function RunEarly() {
-        global $vars;
-        global $auth;
-        if(isset($_POST['AddToCartIngredient']) || isset($_POST['AddToCartBoxes'])) {
-            # id of cart
-            $query = "SELECT BESTELLUNG.BESTELLNR FROM BESTELLUNG WHERE BESTELLUNG.STATUS = :orderstate AND BESTELLUNG.KUNDENNR = :userid";
-            $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
-            $orderid = $this->db->executeSingleRowQuery($query, $params)['BESTELLNR'];
-            if(isset($_POST['AddToCartIngredient'])) {
-                # test if the ingredient is already in the cart
-                $query = "SELECT * FROM BESTELLUNGZUTAT WHERE BESTELLNR=:orderid AND ZUTATENNR=:ingredientid";
-                $params = [':orderid' => $orderid,
-                            ':ingredientid' => $_POST['ZUTATENNR']];
-                if($this->db->executeExists($query, $params)){
-                    # read amount
-                    $query = "SELECT MENGE FROM BESTELLUNGZUTAT WHERE BESTELLNR=:orderid AND ZUTATENNR=:ingredientid";
-                    $params = [':orderid' => $orderid,
-                                ':ingredientid' => $_POST['ZUTATENNR']];
-                    $amount = $this->db->executeSingleRowQuery($query, $params)['MENGE'];
-                    # Update row
-                    $query = "UPDATE BESTELLUNGZUTAT SET MENGE=:amount WHERE BESTELLNR=:orderid AND ZUTATENNR=:ingredientid";
-                    $params = [':orderid' => $orderid,
-                                ':ingredientid' => $_POST['ZUTATENNR'],
-                                ':amount' => $amount + $_POST['amount']];
-                    $this->db->execute($query, $params);
-                } else {
-                    # insert new
-                    $query = "INSERT INTO BESTELLUNGZUTAT (BESTELLNR, ZUTATENNR, MENGE) VALUES (:orderid, :ingredientid, :amount)";
-                    $params = [':orderid' => $orderid,
-                                ':ingredientid' => $_POST['ZUTATENNR'],
-                                ':amount' => $_POST['amount']];
-                    $this->db->execute($query, $params);
-                }
-            }
-        }
-
-    }
-
     public function RunDefault() {
         global $vars;
         global $auth;
