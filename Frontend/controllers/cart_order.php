7792c4e622253e514ab0bf8a27f6283bbf50c9f2
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 08:50:38 +0200

    cart order

---
+++ Frontend/controllers/cart_order.php
@@ -0,0 +1,14 @@
+<?php
+# Needs to be the same as class [NAME]
+new ControllerCartOrder($controllers, $db, ["Content" => "cart_order"]);
+# Be sure to give it a unique name.
+class ControllerCartOrder extends BaseController {
+    public function RunDefault() {
+        global $vars;
+
+
+    }
+}
+
+
+?>
