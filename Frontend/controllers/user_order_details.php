6d4f87bb287a55c44f9481a38cf45d9f943ac579
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:20:23 +0200

    order details

--- Frontend/controllers/user_order_details.php
+++ Frontend/controllers/user_order_details.php
@@ -3,6 +3,12 @@
 new ControllerUserOrderDetails($controllers, $db, ["Content" => "user_order_details"]);
 # Be sure to give it a unique name.
 class ControllerUserOrderDetails extends BaseController {
-
+    public function RunDefault() {
+        global $vars;
+        global $auth;
+        $query = "SELECT BESTELLNR, BESTELLDATUM, RECHNUNGSBETRAG FROM BESTELLUNG WHERE BESTELLNR=:orderid";
+        $params = [":orderid" => $_GET['id']];
+        $vars['orderdetails'] = $this->db->executeQuery($query, $params);
+    }
 }
 ?>
---
+++ Frontend/views/user_order_details.php
@@ -0,0 +1,12 @@
+<table>
+  <tr>
+    <th>Bestellnummer</th>
+    <th>Bestelldatum</th>
+    <th>Betrag</th>
+  </tr>
+  <tr>
+    <td><?php echo $vars['orderdetails']['BESTELLNR']; ?></td>
+    <td><?php echo $vars['orderdetails']['BESTELLDATUM']; ?></td>
+    <td><?php echo $vars['orderdetails']['RECHNUNGSBETRAG']; ?></td>
+  </tr>
+</table>
