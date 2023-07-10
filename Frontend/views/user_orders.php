44378ace6769ebb8ed438bff3258e9b2c7487d46
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:48:30 +0200

    clickable row

--- Frontend/views/user_orders.php
+++ Frontend/views/user_orders.php
@@ -6,8 +6,8 @@
             <td style="text-align: right;">Rechnungsbetrag</td>
         </tr>
     <?php foreach($vars['orders'] as $order) { ?>
-        <tr>
-            <td><a href="/bestellung/<?php echo $order['BESTELLNR']; ?>"><?php echo $order['BESTELLNR']; ?></a></td>
+        <tr onclick="window.location.href = '/bestellung/<?php echo $order['BESTELLNR']; ?>'">
+            <td><?php echo $order['BESTELLNR']; ?></td>
             <td style="text-align: right;"><?php echo $order['BESTELLDATUM']; ?></td>
             <td style="text-align: right;"><?php echo $order['RECHNUNGSBETRAG']; ?></td>
         </tr>
