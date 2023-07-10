ce09abed2131cc7753f000845e37e0e540bcaf80
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 09:53:48 +0200

    user test

--- Frontend/controllers/cart_manager.php
+++ Frontend/controllers/cart_manager.php
@@ -9,42 +9,46 @@ class ControllerCartManager extends BaseController {
         global $vars;
         global $auth;
 
-        # Test if the user has a Cart, otherwise create one.
-        $query = "SELECT * FROM BESTELLUNG WHERE STATUS=:orderstate AND KUNDENNR=:userid";
-        $params = [":orderstate" => 'Warenkorb', ":userid" => $auth->UserID()];
-        if(!$this->db->executeExists($query, $params)) {
-            Logger::log("User has no Cart.");
-            $query = "INSERT INTO BESTELLUNG (KUNDENNR, STATUS)
-                        VALUES (:userid, :orderstate)";
+        if($auth->LoggedIn())
+        {
+
+            # Test if the user has a Cart, otherwise create one.
+            $query = "SELECT * FROM BESTELLUNG WHERE STATUS=:orderstate AND KUNDENNR=:userid";
             $params = [":orderstate" => 'Warenkorb', ":userid" => $auth->UserID()];
-            $this->db->execute($query, $params);
-        } else {
-            Logger::log("User has a Cart.");
+            if(!$this->db->executeExists($query, $params)) {
+                Logger::log("User has no Cart.");
+                $query = "INSERT INTO BESTELLUNG (KUNDENNR, STATUS)
+                            VALUES (:userid, :orderstate)";
+                $params = [":orderstate" => 'Warenkorb', ":userid" => $auth->UserID()];
+                $this->db->execute($query, $params);
+            } else {
+                Logger::log("User has a Cart.");
+            }
+
+            # num items in cart - ingredients
+            $query = "SELECT SUM(BESTELLUNGZUTAT.MENGE) AS Anzahl_Zutaten FROM BESTELLUNG
+                        JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
+                        JOIN BESTELLUNGZUTAT ON BESTELLUNG.BESTELLNR = BESTELLUNGZUTAT.BESTELLNR
+                        WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid";
+
+            $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
+
+            $row = $this->db->executeSingleRowQuery($query, $params);
+
+            $vars['cart_num_items'] = $row['Anzahl_Zutaten'];
+
+            # num items in cart - boxes
+            $query = "SELECT SUM(BESTELLUNGSAMMLUNG.MENGE) AS Anzahl_Zutaten FROM BESTELLUNG
+                        JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
+                        JOIN BESTELLUNGSAMMLUNG ON BESTELLUNG.BESTELLNR = BESTELLUNGSAMMLUNG.BESTELLNR
+                        WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid";
+
+            $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
+
+            $row = $this->db->executeSingleRowQuery($query, $params);
+
+            $vars['cart_num_items'] += $row['Anzahl_Zutaten'];
         }
-
-        # num items in cart - ingredients
-        $query = "SELECT SUM(BESTELLUNGZUTAT.MENGE) AS Anzahl_Zutaten FROM BESTELLUNG
-                    JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
-                    JOIN BESTELLUNGZUTAT ON BESTELLUNG.BESTELLNR = BESTELLUNGZUTAT.BESTELLNR
-                    WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid";
-
-        $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
-
-        $row = $this->db->executeSingleRowQuery($query, $params);
-
-        $vars['cart_num_items'] = $row['Anzahl_Zutaten'];
-
-        # num items in cart - boxes
-        $query = "SELECT SUM(BESTELLUNGSAMMLUNG.MENGE) AS Anzahl_Zutaten FROM BESTELLUNG
-                    JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
-                    JOIN BESTELLUNGSAMMLUNG ON BESTELLUNG.BESTELLNR = BESTELLUNGSAMMLUNG.BESTELLNR
-                    WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid";
-
-        $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
-
-        $row = $this->db->executeSingleRowQuery($query, $params);
-
-        $vars['cart_num_items'] += $row['Anzahl_Zutaten'];
     }
 }
 ?>
