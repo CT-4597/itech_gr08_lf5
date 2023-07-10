b3b57899c1814c98228f6e12ca82abf7412680db
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:21:44 +0200

    form added

--- Frontend/views/user_order_details.php
+++ Frontend/views/user_order_details.php
@@ -1,12 +1,14 @@
-<table>
-  <tr>
-    <th>Bestellnummer</th>
-    <th>Bestelldatum</th>
-    <th>Betrag</th>
-  </tr>
-  <tr>
-    <td><?php echo $vars['orderdetails']['BESTELLNR']; ?></td>
-    <td><?php echo $vars['orderdetails']['BESTELLDATUM']; ?></td>
-    <td><?php echo $vars['orderdetails']['RECHNUNGSBETRAG']; ?></td>
-  </tr>
-</table>
+<form>
+    <table>
+      <tr>
+        <th>Bestellnummer</th>
+        <th>Bestelldatum</th>
+        <th>Betrag</th>
+      </tr>
+      <tr>
+        <td><?php echo $vars['orderdetails']['BESTELLNR']; ?></td>
+        <td><?php echo $vars['orderdetails']['BESTELLDATUM']; ?></td>
+        <td><?php echo $vars['orderdetails']['RECHNUNGSBETRAG']; ?></td>
+      </tr>
+    </table>
+</form>
