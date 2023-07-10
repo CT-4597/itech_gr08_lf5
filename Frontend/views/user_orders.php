91fa0487bdaf99e09032ca805fc46afe39f92a60
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:22:20 +0200

    orders

--- Frontend/views/user_orders.php
+++ Frontend/views/user_orders.php
@@ -1,14 +1,16 @@
-<table>
-    <tr>
-        <td>Bestellnummer</td>
-        <td style="text-align: right;">Bestelldatum</td>
-        <td style="text-align: right;">Rechnungsbetrag</td>
-    </tr>
-<?php foreach($vars['orders'] as $order) { ?>
-    <tr>
-        <td><a href="/bestellung/<?php echo $order['BESTELLNR']; ?>"><?php echo $order['BESTELLNR']; ?></a></td>
-        <td style="text-align: right;"><?php echo $order['BESTELLDATUM']; ?></td>
-        <td style="text-align: right;"><?php echo $order['RECHNUNGSBETRAG']; ?></td>
-    </tr>
- <?php } ?>
- </table>
+<form>
+    <table>
+        <tr>
+            <td>Bestellnummer</td>
+            <td style="text-align: right;">Bestelldatum</td>
+            <td style="text-align: right;">Rechnungsbetrag</td>
+        </tr>
+    <?php foreach($vars['orders'] as $order) { ?>
+        <tr>
+            <td><a href="/bestellung/<?php echo $order['BESTELLNR']; ?>"><?php echo $order['BESTELLNR']; ?></a></td>
+            <td style="text-align: right;"><?php echo $order['BESTELLDATUM']; ?></td>
+            <td style="text-align: right;"><?php echo $order['RECHNUNGSBETRAG']; ?></td>
+        </tr>
+     <?php } ?>
+     </table>
+</form>
