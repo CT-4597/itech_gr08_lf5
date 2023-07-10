478982ed227a5024466ecc38a0f12cbfad2b73a7
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 09:50:36 +0200

    total price

--- Frontend/controllers/cart_order.php
+++ Frontend/controllers/cart_order.php
@@ -7,9 +7,35 @@ class ControllerCartOrder extends BaseController {
         global $vars;
         global $auth;
 
+        $query = "SELECT FORMAT(SUM(Gesamtpreis), 2) AS Gesamtsumme
+                    FROM (
+                      SELECT FORMAT(SUM((ZUTAT.NETTOPREIS * BESTELLUNGZUTAT.MENGE)), 2) as Gesamtpreis
+                      FROM BESTELLUNG
+                      JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
+                      JOIN BESTELLUNGZUTAT ON BESTELLUNG.BESTELLNR = BESTELLUNGZUTAT.BESTELLNR
+                      JOIN ZUTAT ON BESTELLUNGZUTAT.ZUTATENNR = ZUTAT.ZUTATENNR
+                      WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid
+
+                      UNION ALL
+
+                      SELECT FORMAT((SUM(ZUTAT.NETTOPREIS * SAMMLUNGZUTAT.ZUTATENMENGE) / 100 * (100 - SAMMLUNG.RABATT)) * BESTELLUNGSAMMLUNG.MENGE, 2) AS Gesamtpreis
+                      FROM BESTELLUNG
+                      JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
+                      LEFT JOIN BESTELLUNGSAMMLUNG ON BESTELLUNG.BESTELLNR = BESTELLUNGSAMMLUNG.BESTELLNR
+                      LEFT JOIN SAMMLUNG ON BESTELLUNGSAMMLUNG.SAMMLUNGSNR = SAMMLUNG.SAMMLUNGSNR
+                      LEFT JOIN SAMMLUNGZUTAT ON SAMMLUNG.SAMMLUNGSNR = SAMMLUNGZUTAT.SAMMLUNGSNR
+                      LEFT JOIN ZUTAT ON SAMMLUNGZUTAT.ZUTATENNR = ZUTAT.ZUTATENNR
+                      WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid
+                      GROUP BY SAMMLUNG.SAMMLUNGSNR, SAMMLUNG.SAMMLUNGSBEZEICHNUNG
+                    ) AS Subquery";
+
+        $params = [":orderstate" => 'Warenkorb',
+                    ":userid" => $auth->UserID()];
+        $total_price = $this->db->executeSingleRowQuery($query, $params)['Gesamtsumme'];
+
         $query = "UPDATE BESTELLUNG SET BESTELLDATUM=:orderdate, RECHNUNGSBETRAG=:total_price, STATUS=:neworderstate WHERE STATUS=:orderstate AND KUNDENNR=:userid";
         $params = [":orderdate" => date('Y-m-d'),
-                    ":total_price" => 0,
+                    ":total_price" => $total_price,
                     ":neworderstate" => 'Bestellt',
                     ":orderstate" => 'Warenkorb',
                     ":userid" => $auth->UserID()];
