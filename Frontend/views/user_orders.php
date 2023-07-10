022666097a60e52d37e5d7d55d973d9839098a8e
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:16:10 +0200

    user orders added link

--- Frontend/views/user_orders.php
+++ Frontend/views/user_orders.php
@@ -6,7 +6,7 @@
     </tr>
 <?php foreach($vars['orders'] as $order) { ?>
     <tr>
-        <td><?php echo $order['BESTELLNR']; ?></td>
+        <td><a href="/bestellung/<?php echo $order['BESTELLNR']; ?>"><?php echo $order['BESTELLNR']; ?></a></td>
         <td style="text-align: right;"><?php echo $order['BESTELLDATUM']; ?></td>
         <td style="text-align: right;"><?php echo $order['RECHNUNGSBETRAG']; ?></td>
     </tr>
