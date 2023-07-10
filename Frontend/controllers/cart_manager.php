662740e9e619e530c435e4179c19e739661aa3e5
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 07:02:54 +0200

    num items

--- Frontend/controllers/cart_manager.php
+++ Frontend/controllers/cart_manager.php
@@ -8,32 +8,17 @@ class ControllerCartManager extends BaseController {
         global $vars;
         global $auth;
 
-        # boxes in cart
-        $query_cart_ingredients = "SELECT ZUTAT.ZUTATENNR, ZUTAT.BEZEICHNUNG, ZUTAT.NETTOPREIS, ZUTAT.EINHEIT, BESTELLUNGZUTAT.MENGE, FORMAT((ZUTAT.NETTOPREIS * BESTELLUNGZUTAT.MENGE), 2) as GESAMTPREIS
-                                    FROM BESTELLUNG
-                                    JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
-                                    JOIN BESTELLUNGZUTAT ON BESTELLUNG.BESTELLNR = BESTELLUNGZUTAT.BESTELLNR
-                                    JOIN ZUTAT ON BESTELLUNGZUTAT.ZUTATENNR = ZUTAT.ZUTATENNR
-                                    WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid";
+        # num items in cart - ingredients
+        $query = "SELECT COUNT(*) AS Anzahl_Zutaten FROM BESTELLUNG
+                    JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
+                    JOIN BESTELLUNGZUTAT ON BESTELLUNG.BESTELLNR = BESTELLUNGZUTAT.BESTELLNR
+                    WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid";
+
         $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
 
-        # $vars['cart_ingeredients'] = $this->db->executeQuery($query_cart_ingredients, $params);
+        $row = $this->db->executeSingleRowQuery($query_cart_boxes, $params);
 
-        $query_cart_boxes = "SELECT SAMMLUNG.SAMMLUNGSNR, SAMMLUNG.SAMMLUNGSBEZEICHNUNG, BESTELLUNGSAMMLUNG.MENGE,
-                                FORMAT((SUM(ZUTAT.NETTOPREIS * SAMMLUNGZUTAT.ZUTATENMENGE) / 100 * (100 - SAMMLUNG.RABATT)), 2) AS EINZELPREIS,
-                                FORMAT((SUM(ZUTAT.NETTOPREIS * SAMMLUNGZUTAT.ZUTATENMENGE) / 100 * (100 - SAMMLUNG.RABATT)) * BESTELLUNGSAMMLUNG.MENGE, 2) AS GESAMTPREIS
-                                FROM BESTELLUNG
-                                JOIN KUNDE ON BESTELLUNG.KUNDENNR = KUNDE.KUNDENNR
-                                LEFT JOIN BESTELLUNGSAMMLUNG ON BESTELLUNG.BESTELLNR = BESTELLUNGSAMMLUNG.BESTELLNR
-                                LEFT JOIN SAMMLUNG ON BESTELLUNGSAMMLUNG.SAMMLUNGSNR = SAMMLUNG.SAMMLUNGSNR
-                                LEFT JOIN SAMMLUNGZUTAT ON SAMMLUNG.SAMMLUNGSNR = SAMMLUNGZUTAT.SAMMLUNGSNR
-                                LEFT JOIN ZUTAT ON SAMMLUNGZUTAT.ZUTATENNR = ZUTAT.ZUTATENNR
-                                WHERE BESTELLUNG.STATUS = :orderstate AND KUNDE.KUNDENNR = :userid GROUP BY SAMMLUNG.SAMMLUNGSNR, SAMMLUNG.SAMMLUNGSBEZEICHNUNG";
-        $params = [':orderstate' => 'Warenkorb', ':userid' => $auth->UserID()];
-
-        # $vars['cart_boxes'] = $this->db->executeQuery($query_cart_boxes, $params);
-
-        $vars['cart_num_items'] = 10;
+        $vars['cart_num_items'] = $row['Anzahl_Zutaten'];
     }
 }
 ?>
