d37adae5c9af3aa248470cd522f2517127176c4b
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:30:10 +0200

    typo

--- Frontend/views/user_order_details.php
+++ Frontend/views/user_order_details.php
@@ -13,7 +13,7 @@
     </table>
     <table>
 
-    <?php foreach($vars['order_ingeredients'] as $ingredient) { ?>
+    <?php foreach($vars['order_ingredients'] as $ingredient) { ?>
         <tr>
             <td><?php echo $ingredient['ZUTATENNR']; ?></td>
             <td><?php echo $ingredient['BEZEICHNUNG']; ?></td>
