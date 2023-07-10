5f5316228b0d042c89382273aca9ced0a5a42c4f
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-10 10:28:22 +0200

    order details

--- Frontend/views/user_order_details.php
+++ Frontend/views/user_order_details.php
@@ -11,4 +11,31 @@
         <td><?php echo $vars['orderdetails']['RECHNUNGSBETRAG']; ?></td>
       </tr>
     </table>
+    <table>
+
+    <?php foreach($vars['order_ingeredients'] as $ingredient) { ?>
+        <tr>
+            <td><?php echo $ingredient['ZUTATENNR']; ?></td>
+            <td><?php echo $ingredient['BEZEICHNUNG']; ?></td>
+            <td><?php echo $ingredient['MENGE']; ?></td>
+            <td><?php echo $ingredient['EINHEIT']; ?></td>
+            <td><?php echo $ingredient['NETTOPREIS']; ?></td>
+            <td><?php echo $ingredient['GESAMTPREIS']; ?></td>
+        </tr>
+    <?php } ?>
+    </table>
+    <br>
+    <table>
+
+    <?php foreach($vars['order_boxes'] as $box) { ?>
+        <tr>
+            <td><?php echo $box['SAMMLUNGSNR']; ?></td>
+            <td><?php echo $box['SAMMLUNGSBEZEICHNUNG']; ?></td>
+            <td><?php echo $box['MENGE']; ?></td>
+            <td><?php echo $box['EINZELPREIS']; ?></td>
+            <td><?php echo $box['GESAMTPREIS']; ?></td>
+        </tr>
+    <?php } ?>
+    </table>
+
 </form>
