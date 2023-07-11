b23b04f7f977c3f7b5912635348888b64119ef7a
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-11 08:10:59 +0200

    testing

--- Frontend/includes/helper.php
+++ Frontend/includes/helper.php
@@ -105,10 +105,10 @@ function validateDate(&$err, $date, $min = 1900) {
 $errors = [];
 if(isset($_POST['validate'])){
     $err = False;
-    # $err = $err or !validatePassword($errors['pw'], $_POST['pw1'], $_POST['pw2']);
+    $err = $err or !validatePassword($errors['pw'], $_POST['pw1'], $_POST['pw2']);
     # $err = $err or !validateDate($errors['date'], $_POST['date']);
     # $err = $err or !validateEmail($errors['email'], $_POST['email']);
-    $err = $err or !validateString($errors['firstname'], $_POST['firstname'], '/^[A-Za-z]{3,20}$/', 'Ungültiger Name.');
+    # $err = $err or !validateString($errors['firstname'], $_POST['firstname'], '/^[A-Za-z]{3,20}$/', 'Ungültiger Name.');
 
     if($err) {
         echo "Some Error: " . implode(' - ', array_keys($errors));
