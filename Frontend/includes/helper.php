73318ae6917f1217a374598d3e80eb72b7f44e08
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-11 07:57:53 +0200

    msg

--- Frontend/includes/helper.php
+++ Frontend/includes/helper.php
@@ -106,11 +106,11 @@ $errors = [];
 if(isset($_POST['validate'])){
     $err = False;
     # $err = $err xor validatePassword($errors['pw'], $_POST['pw1'], $_POST['pw2']);
-    $err = $err || validateDate($errors['date'], $_POST['date']);
+    $err = $err or validateDate($errors['date'], $_POST['date']);
     # $err = $err xor validateEmail($errors['email'], $_POST['email']);
     # $err = $err xor validateString($errors['firstname'], $_POST['firstname'], '/^[A-Za-z]{3,20}$/', 'Ungültiger Name.');
 
-    if(!$err) {
+    if($err) {
         echo "looks good";
     } else {
         echo implode(' - ', array_keys($errors));
