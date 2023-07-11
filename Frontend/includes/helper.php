5399084e0144a6b88a635b9d958fecd9fcbb3fdc
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-11 08:09:11 +0200

    maybe

--- Frontend/includes/helper.php
+++ Frontend/includes/helper.php
@@ -111,9 +111,9 @@ if(isset($_POST['validate'])){
     $err = $err or !validateString($errors['firstname'], $_POST['firstname'], '/^[A-Za-z]{3,20}$/', 'Ungültiger Name.');
 
     if($err) {
-        echo "looks good";
-    } else {
         echo "Some Error: " . implode(' - ', array_keys($errors));
+    } else {
+        echo "looks good";
     }
 
 }
